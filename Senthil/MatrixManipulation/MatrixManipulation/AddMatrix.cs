4341490c8abcab5a9a0d72203c74ef9e71efbe72
senpost <senpost@gmail.com>
2014-01-16 09:21:26 -0800

    Added sample addition of matrix.

---
+++ Senthil/MatrixManipulation/MatrixManipulation/AddMatrix.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixManipulation
+{
+    class AddMatrix
+    {
+        public int[,] Add3By3(int[,] matrix1, int[,] matrix2)
+        {            
+            int[,] total = new int[3, 3];
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    total[i, j] = matrix1[i, j] + matrix2[i, j];
+                }
+            }
+
+            return total;
+
+        }
+    }
+}
---
+++ Senthil/MatrixManipulation/MatrixManipulation/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixManipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix1 = new int[3, 3]
+            {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+            };
+
+            int[,] matrix2 = new int[3, 3]
+            {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+            };
+
+            AddMatrix matrixAddition = new AddMatrix();
+            int[,] total = matrixAddition.Add3By3(matrix1, matrix2);
+            Print3By3Matrix(total);
+        }
+
+        static void Print3By3Matrix(int[,] matrix)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.WriteLine(matrix[i,j]);
+                }
+            }
+        }
+    }
+}
