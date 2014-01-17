83a496aa66aefe57c5fb245ea327a322913b0238
saranpost <saranpost@gmail.com>
2014-01-17 17:47:38 +0530

    Addition Matrix

---
+++ Saran/MatrixManipulation/MainClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixManipulation
+{
+    class MainClass
+    {
+        public static void main(string[] args)
+        {
+			//Adding test command.
+            int[,] matrix1 = new int[3, 3]
+            {
+                {3,5,6},
+                {2,5,7},
+                {1,7,9},
+            };
+            int[,] matrix2 = new int[3, 3]
+            {
+                {3,5,6},
+                {2,5,7},
+                {1,7,9},
+            };
+            Program obj = new Program();
+            int[,] result = obj.AdditionMatrix(matrix1, matrix2);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Saran/MatrixManipulation/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixManipulation
+{
+    class Program
+    {
+        public int[,] AdditionMatrix(int[,] matrix1, int[,] matrix2)
+        {
+            int i, j;
+            int[,] result = new int[3, 3];
+            for (i = 0; i < matrix1.Length; i++)
+            {
+                for (j = 0; j < matrix2.Length; j++)
+                {
+                    result[i, j] = matrix1[i, j] + matrix2[i, j];
+                }
+            } return result;
+
+
+        }
+    }
+}
