27e98196214c5cb250b2b4a0f350392261ceb072
senpost <senpost@gmail.com>
2014-01-17 19:07:07 -0800

    Added student database sample project.

---
+++ Senthil/StudentDatabaseUsingArray/StudentDatabaseUsingArray/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDatabaseUsingArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StudentDatabase db = new StudentDatabase(5);
+            db.AddStudent("1", "Noah", 90, 92, 90);
+            db.AddStudent("2", "Saran", 90, 90, 90);
+            db.PrintInfo("7");
+
+            //Find all students matching Mark1
+            //Find all students matching total mark
+            //Print each students max mark
+            //Print each students min mark
+            //Student with highest total, First rank student
+
+            string m1 = "59";
+            string m2 = "-99";
+
+            int integerM1 = Convert.ToInt32(m1);
+            int integerM2 = Convert.ToInt32(m2);
+
+            //if(m1 > m2) Error
+
+            if (integerM1 > integerM2)
+            {
+
+            }
+
+            
+        }
+    }
+}
---
+++ Senthil/StudentDatabaseUsingArray/StudentDatabaseUsingArray/StudentDatabase.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDatabaseUsingArray
+{
+    class StudentDatabase
+    {
+        private string[,] students;
+        //Id, Name, Mark1, Mark2, Mark3
+        private const int noOfFields = 5;
+        private int recordPointer = 0;
+
+        public StudentDatabase(int noOfStudents)
+        {
+            students = new string[noOfStudents, noOfFields];
+        }
+
+        public void PrintInfo(string id)
+        {
+            for (int i = 0; i < recordPointer; i++)
+            {
+                if(students[i,0] == id)
+                {
+                    Console.WriteLine("Student Info: Id:{0}, Name:{1}", students[i,0], students[i,1]);
+                    return;
+                }
+            }
+
+            Console.WriteLine("No matching student found");
+        }
+
+        public void AddStudent(string id, string name, int mark1, int mark2, int mark3)
+        {
+            //Handele max students exception
+
+            students[recordPointer, 0] = id;
+            students[recordPointer, 1] = name;
+            students[recordPointer, 2] = mark1.ToString();
+            students[recordPointer, 3] = mark2.ToString();
+            students[recordPointer, 4] = mark3.ToString();
+            recordPointer++;
+        }
+    }
+}
