1e71ad7aa9ea5f327ff66f3ddf2801ee7673477e
Rajaramachandran <rajaramachandran88@gmail.com>
2014-01-19 07:43:21 -0800

    Added StudentPrint All Record

--- Raja/StudentDatabaseUsingArray/StudentDatabaseUsingArray/Program.cs
+++ Raja/StudentDatabaseUsingArray/StudentDatabaseUsingArray/Program.cs
@@ -10,6 +10,18 @@ namespace StudentDatabaseUsingArray
     {
         static void Main(string[] args)
         {
+            // Store Student Info
+            StudentDatabase db = new StudentDatabase(4);
+            db.AddStudent(1, "Saran",43, 76, 98);
+            db.AddStudent(2, "Raja", 54, 79, 32);
+            db.AddStudent(3, "raj",54, 60, 45);
+             
+            // Retrive Student Info
+            db.PrintAllStudentInfo();
+            db.PrintStudentNameById();
+            
+
+
         }
     }
 }
