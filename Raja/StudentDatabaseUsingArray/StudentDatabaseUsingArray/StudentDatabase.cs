c03daceff0aac420c1728ecbceb214c723a5edc8
Rajaramachandran <rajaramachandran88@gmail.com>
2014-01-19 17:51:33 -0800

    Student Database

---
+++ Raja/StudentDatabaseUsingArray/StudentDatabaseUsingArray/StudentDatabase.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDatabaseUsingArray
+{
+    class StudentDatabase
+    {
+        string[,] students = new string[2, 5];
+        int AddedStudentRecord = 0;
+
+        public StudentDatabase(int totalStudents)
+        {
+            students = new string[totalStudents, 5];
+        }
+
+        public void AddStudent(int id, string name, int mark1, int mark2, int mark3)
+        {
+            students[AddedStudentRecord, 0] = id.ToString();
+            students[AddedStudentRecord, 1] = name;
+            students[AddedStudentRecord, 2] = mark1.ToString();
+            students[AddedStudentRecord, 3] = mark2.ToString();
+            students[AddedStudentRecord, 4] = mark3.ToString();
+            AddedStudentRecord++;
+        }
+
+        public void PrintAllStudentInfo()
+        {
+            Console.WriteLine("All Student Infomatiom");
+            for (int i = 0; i < AddedStudentRecord; i++)
+            {
+                Console.WriteLine("Id:{0},Name:{1},Mark1:{2},Mark2:{3},Mark3:{4}", students[i, 0], students[i, 1], students[i, 2], students[i, 3], students[i, 4]);
+            }
+        }
+
+        public void PrintStudentNameById()
+        {
+            string StudentId;
+            Console.WriteLine("Enter the Student Id:");
+            StudentId = (Console.ReadLine());
+            for (int i = 0; i < AddedStudentRecord; i++)
+            {
+                if (students[i, 0] == StudentId)
+                {
+                    Console.WriteLine("Id:{0},Name:{1},Mark1:{2},Mark2:{3},Mark3:{4}", students[i, 0], students[i, 1], students[i, 2], students[i, 3], students[i, 4]);
+                }
+            }
+
+        }
+    }
+}
---
+++ Raja/StudentDatabaseUsingArray/StudentDatabaseUsingArray/StudentDetails.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDatabaseUsingArray
+{
+    class StudentDatabase
+    {
+        String[,] students;
+        int noOfStudentAdded = 0;
+        int noOfStudents;
+
+        public StudentDatabase(int totalStudents)
+        {
+            students = new String[totalStudents, 5];
+            this.noOfStudents = totalStudents;
+        }
+
+        public void AddStudent(int id, string name, int mark1, int mark2, int mark3)
+        {
+            students[noOfStudentAdded, 0] = id.ToString();
+            students[noOfStudentAdded, 1] = name;
+            students[noOfStudentAdded, 2] = mark1.ToString();
+            students[noOfStudentAdded, 3] = mark2.ToString();
+            students[noOfStudentAdded, 4] = mark3.ToString();
+            noOfStudentAdded++;
+        }
+
+        public void PrintAllStudentsInfo()
+        {
+
+            for (int i = 0; i < noOfStudentAdded; i++)
+            {
+                Console.WriteLine("Id:{0} Name:{1} Mark1:{2} Mark2:{3} Mark3:{4}", students[i, 0], students[i, 1], students[i, 2], students[i, 3], students[i, 4]);
+
+            }
+
+        }
+
+
+        // input 1 saran 43 54 76 
+        //      2 raj   65 65 98 
+
+        //output 1 saran 43 54 76 
+        //       2 raj   65 65 98 
+
+
+
+
+    }
+}
+
