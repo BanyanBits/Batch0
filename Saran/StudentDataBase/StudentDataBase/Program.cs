08da0cff971e4bc57ea3f0e75dafda8cd3d0b9d8
saranpost <saranpost@gmail.com>
2014-01-19 22:17:56 -0800

    Added student DataBase Application, Function with Student Name BY Id ,Total Mark And its Average of the Student.

---
+++ Saran/StudentDataBase/StudentDataBase/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDataBase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Store Student Info
+            StudentDatabaseFunction db = new StudentDatabaseFunction(4);
+            db.AddStudent(1, "Saran", 43, 76, 98);
+            db.AddStudent(2, "Raja", 54, 79, 32);
+            db.AddStudent(3, "raj", 54, 60, 45);
+
+            // Retrive Student Info
+            db.PrintAllStudentInfo();
+            db.PrintStudentNameById();
+            db.StudentTotalMark();
+
+        }
+    }
+}
+
+    
---
+++ Saran/StudentDataBase/StudentDataBase/StudentDatabaseFunction.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDataBase
+{
+    class StudentDatabaseFunction
+    {
+        string[,] students = new string[2, 5];
+        int AddedStudentRecord = 0;
+
+        public StudentDatabaseFunction(int totalStudents)
+        {
+            students = new string[totalStudents, 5];
+        }
+
+        public void AddStudent(int id, string name, int mark1, int mark2, int mark3)
+        {
+            students[AddedStudentRecord, 0] = id.ToString();
+            students[AddedStudentRecord, 1] = name;
+            students[AddedStudentRecord, 2] = mark1.ToString();
+            students[AddedStudentRecord, 3] = mark2.ToString();
+            students[AddedStudentRecord, 4] = mark3.ToString();
+            AddedStudentRecord++;
+        }
+
+        public void PrintAllStudentInfo()
+        {
+            Console.WriteLine("\t\nAll Student Infomatiom");
+            Console.WriteLine("*** ******* **********\n");
+            for (int i = 0; i < AddedStudentRecord; i++)
+            {
+                Console.WriteLine("Id:{0},Name:{1},Mark1:{2},Mark2:{3},Mark3:{4}", students[i, 0], students[i, 1], students[i, 2], students[i, 3], students[i, 4]);
+            }
+        }
+
+        public void PrintStudentNameById()
+        {
+            string StudentId;
+            Console.WriteLine("Enter the Student Id:");
+            StudentId = (Console.ReadLine());
+            for (int i = 0; i < AddedStudentRecord; i++)
+            {
+                if (students[i, 0] == StudentId)
+                {
+                    Console.WriteLine("Id:{0},Name:{1},Mark1:{2},Mark2:{3},Mark3:{4}", students[i, 0], students[i, 1], students[i, 2], students[i, 3], students[i, 4]);
+                }
+            }
+            Console.WriteLine("Enter the Student Valid Id Number");
+
+
+
+
+        }
+
+        public void StudentTotalMark()
+        {
+            for (int i = 0; i < AddedStudentRecord; i++)
+            {
+                int total = Convert.ToInt32(students[i, 2]) + Convert.ToInt32(students[i, 3]) + Convert.ToInt32(students[i, 4]);
+                float average = total / 3;
+                Console.WriteLine("Id:{0},Name:{1},Mark1:{2},Mark2:{3},Mark3:{4},Total : {5},Average : {6}", students[i, 0], students[i, 1], students[i, 2], students[i, 3], students[i, 4], total, average);
+
+            }
+
+        }
+
+    }
+}
