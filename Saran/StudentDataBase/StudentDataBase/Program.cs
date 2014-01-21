6c6be2bd4b636ae7c248f71c502ed7112bf789ed
saranpost <saranpost@gmail.com>
2014-01-21 11:37:05 -0800

    Added Function For Find Student By Mark and Address

--- Saran/StudentDataBase/StudentDataBase/Program.cs
+++ Saran/StudentDataBase/StudentDataBase/Program.cs
@@ -11,18 +11,25 @@ namespace StudentDataBase
         static void Main(string[] args)
         {
             // Store Student Info
-            StudentDatabaseFunction db = new StudentDatabaseFunction(4);
-            db.AddStudent(1, "Saran", 99, 76, 98);
-            db.AddStudent(2, "Raja", 54, 79, 32);
-            db.AddStudent(3, "Raj", 54, 60, 75);
+
+            StudentDatabaseFunction db = new StudentDatabaseFunction(6);
+            db.AddStudent(1, "Saran", 54, 76, 98,"Salem");
+            db.AddStudent(2, "Raja", 45, 79, 32,"Thrchy");
+            db.AddStudent(3, "Raj", 87, 60, 45,"Chennai");
+            db.AddStudent(1, "Ram", 43, 76, 98, "Salem");
+            db.AddStudent(2, "Muthu", 90, 79, 32, "Thrchy");
+            db.AddStudent(3, "Mani", 54, 60, 45, "Chennai");
 
             // Retrive Student Info
-            db.PrintAllStudentInfo();
-            db.StudentTotalMarkAndAverage();
-            db.MaxMarkOfStudent();
-            db.MinimumMarkOfStudent();
-            db.PrintStudentNameById();
-            db.PassOrFail();
+
+            //db.PrintAllStudentInfo();
+            //db.StudentTotalMarkAndAverage();
+            //db.MaxMarkOfStudent();
+            //db.MinimumMarkOfStudent();
+            //db.PrintStudentNameById();
+            //db.PassOrFail();
+            db.FindStudentByMark();
+
         }
     }
 }
--- Saran/StudentDataBase/StudentDataBase/StudentDatabaseFunction.cs
+++ Saran/StudentDataBase/StudentDataBase/StudentDatabaseFunction.cs
@@ -9,21 +9,22 @@ namespace StudentDataBase
     class StudentDatabaseFunction
     {
         string[,] students;
-        int noStudentsAdded = 0;
+        int AddedStudentRecord = 0;
 
         public StudentDatabaseFunction(int totalStudents)
         {
-            students = new string[totalStudents, 5];
+            students = new string[totalStudents, 6];
         }
 
-        public void AddStudent(int id, string name, int mark1, int mark2, int mark3)
+        public void AddStudent(int id, string name, int mark1, int mark2, int mark3, string district)
         {
-            students[noStudentsAdded, 0] = id.ToString();
-            students[noStudentsAdded, 1] = name;
-            students[noStudentsAdded, 2] = mark1.ToString();
-            students[noStudentsAdded, 3] = mark2.ToString();
-            students[noStudentsAdded, 4] = mark3.ToString();
-            noStudentsAdded++;
+            students[AddedStudentRecord, 0] = id.ToString();
+            students[AddedStudentRecord, 1] = name;
+            students[AddedStudentRecord, 2] = mark1.ToString();
+            students[AddedStudentRecord, 3] = mark2.ToString();
+            students[AddedStudentRecord, 4] = mark3.ToString();
+            students[AddedStudentRecord, 5] = district;
+            AddedStudentRecord++;
         }
 
         public void PrintAllStudentInfo()
@@ -31,12 +32,12 @@ namespace StudentDataBase
             Console.WriteLine("\t\nAll Student Infomatiom");
             Console.WriteLine("*** ******* **********\n");
 
-            Console.WriteLine("\n Id\tName\tMark1\tMark2\tMark3");
-            Console.WriteLine(" **\t****\t*****\t*****\t*****\n");
+            Console.WriteLine("\n Id\tName\tMark1\tMark2\tMark3\tDistrict");
+            Console.WriteLine(" **\t****\t*****\t*****\t*****\t**********\n");
 
-            for (int i = 0; i < noStudentsAdded; i++)
+            for (int i = 0; i < AddedStudentRecord; i++)
             {
-                Console.WriteLine(" {0}\t{1}\t{2}\t{3}\t{4}", students[i, 0], students[i, 1], students[i, 2], students[i, 3], students[i, 4]);
+                Console.WriteLine(" {0}\t{1}\t{2}\t{3}\t{4}\t{5}", students[i, 0], students[i, 1], students[i, 2], students[i, 3], students[i, 4], students[i, 5]);
             }
         }
 
@@ -46,28 +47,29 @@ namespace StudentDataBase
             Console.Write("\nEnter the Student Id: ");
             StudentId = (Console.ReadLine());
             Console.WriteLine("\n\n Id\t Name\t Mark1 \t Mark2\t Mark3");
-            for (int i = 0; i < noStudentsAdded; i++)
+            for (int i = 0; i < AddedStudentRecord; i++)
             {
                 if (students[i, 0] == StudentId)
                 {
                     Console.WriteLine("\n {0}\t {1}\t {2}\t {3}\t {4}", students[i, 0], students[i, 1], students[i, 2], students[i, 3], students[i, 4]);
-                    return;
                 }
             }
             Console.WriteLine("Enter the Student Valid Id Number");
+
         }
 
         public void StudentTotalMarkAndAverage()
         {
             Console.WriteLine("\n\n Id\tName\tMark1\tMark2\tMark3\tTotal\tAverage");
             Console.WriteLine(" **\t****\t*****\t*****\t*****\t*****\t*******\n");
-            for (int i = 0; i < noStudentsAdded; i++)
+            for (int i = 0; i < AddedStudentRecord; i++)
             {
                 int total = Convert.ToInt32(students[i, 2]) + Convert.ToInt32(students[i, 3]) + Convert.ToInt32(students[i, 4]);
                 float average = total / 3;
                 Console.WriteLine(" {0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}", students[i, 0], students[i, 1], students[i, 2], students[i, 3], students[i, 4], total, average);
 
             }
+
         }
 
         public void MaxMarkOfStudent()
@@ -75,20 +77,22 @@ namespace StudentDataBase
             Console.WriteLine("\n\n Id \t Name \t MaximumMark");
             Console.WriteLine(" **\t ****\t ***********");
 
-            for (int i = 0; i < noStudentsAdded; i++)
+            for (int i = 0; i < AddedStudentRecord; i++)
             {
                 int mark1 = Convert.ToInt32(students[i, 2]);
                 int mark2 = Convert.ToInt32(students[i, 3]);
                 int mark3 = Convert.ToInt32(students[i, 4]);
 
-                //m1 = 95, m2 = 60, m3 = 91
-
-                if ((mark1 > mark2) && (mark1 > mark3))
+                if (mark1 > mark2)
+                    if (mark1 > mark3)
                         Console.WriteLine(" {0} \t{1} \t {2}", students[i, 0], students[i, 1], mark1);
-                else if (mark2 > mark3)
+
+                if (mark2 > mark3)
                     Console.WriteLine(" {0} \t{1} \t {2}", students[i, 0], students[i, 1], mark2);
+
                 else
                     Console.WriteLine(" {0} \t{1} \t {2}", students[i, 0], students[i, 1], mark3);
+
             }
         }
 
@@ -97,44 +101,75 @@ namespace StudentDataBase
             Console.WriteLine("\n\n Id \t Name \t MinimumOfMark");
             Console.WriteLine(" **\t ****\t *************");
 
-            for (int i = 0; i < noStudentsAdded; i++)
+            for (int i = 0; i < AddedStudentRecord; i++)
             {
                 int mark1 = Convert.ToInt32(students[i, 2]);
                 int mark2 = Convert.ToInt32(students[i, 3]);
                 int mark3 = Convert.ToInt32(students[i, 4]);
 
-                if ((mark1 < mark2) && (mark1 < mark3))
-                    Console.WriteLine(" {0} \t{1} \t {2}", students[i, 0], students[i, 1], mark1);
-                else if (mark2 < mark3)
-                    Console.WriteLine(" {0} \t{1} \t {2}", students[i, 0], students[i, 1], mark2);
-                else
-                    Console.WriteLine(" {0} \t{1} \t {2}", students[i, 0], students[i, 1], mark3);
+                if (mark1 < mark2)
+                    if (mark1 < mark3)
+                        Console.WriteLine(" {0} \t{1} \t {2}", students[i, 0], students[i, 1], mark1);
+
+                    else if (mark2 < mark3)
+
+                        Console.WriteLine(" {0} \t{1} \t {2}", students[i, 0], students[i, 1], mark2);
+
+                    else
+                        Console.WriteLine(" {0} \t{1} \t {2}", students[i, 0], students[i, 1], mark3);
+
             }
         }
 
-
         public void PassOrFail()
         {
+            string result;
             Console.WriteLine("\n\n Id\t Name \t Result");
             Console.WriteLine(" **\t **** \t ******\n");
-            for (int i = 0; i < noStudentsAdded; i++)
+            for (int i = 0; i < AddedStudentRecord; i++)
             {
                 int mark1 = Convert.ToInt32(students[i, 2]);
                 int mark2 = Convert.ToInt32(students[i, 3]);
                 int mark3 = Convert.ToInt32(students[i, 4]);
 
-                string result;
                 if (mark1 >= 40 && mark2 >= 40 && mark3 >= 40)
                 {
                     result = "Pass";
-                    Console.WriteLine(" {0} \t {1} \t {2}", students[i, 0], students[i, 1],result);
+                    Console.WriteLine(" {0} \t {1} \t {2}", students[i, 0], students[i, 1], result);
                 }
                 else
                 {
                     result = "Fail";
-                    Console.WriteLine(" {0} \t {1} \t {2}", students[i, 0], students[i, 1],result);
+                    Console.WriteLine(" {0} \t {1} \t {2}", students[i, 0], students[i, 1], result);
                 }
             }
+
+
         }
+
+        public void FindStudentByMark()
+        {
+            Console.WriteLine("\n\t\t To Find Student By Mark");
+            Console.WriteLine("\t\t ** **** ******* ** ****\n");
+
+            Console.Write("Enter the Student Mark :  ");
+            string mark = Console.ReadLine();
+
+            Console.WriteLine("\n Id \t Name \t Mark1 \t Mark2 \t Mark3 \t Address");
+            Console.WriteLine(" ** \t **** \t ***** \t ***** \t ***** \t *******");
+
+            for (int i = 0; i < AddedStudentRecord; i++)
+            {
+                if ((students[i, 2] == mark) || (students[i, 3] == mark) || (students[i, 4] == mark))
+                {
+                    Console.WriteLine(" {0}\t {1}\t {2}\t {3}\t {4} \t {5}", students[i, 0], students[i, 1], students[i, 2], students[i, 3], students[i, 4], students[i, 5]);
+
+                }
+            }
+            Console.WriteLine("Enter the Student Valid Mark");
+        }
+
+
+
     }
 }
