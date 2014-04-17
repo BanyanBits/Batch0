c97017150d477eeb01cfa23b498a7bea60689441
Rajaramachandran <rajaramachandran88@gmail.com>
2014-04-18 00:51:03 +0530

    Added Student DataBase Using Array

---
+++ Raja/StudentDataBaseUsingArrayObjectList/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDatabaseUsingArrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StudentDb db= new StudentDb(10);
+            db.AddStudent(1, "raja", 19, 67, 99);
+            db.AddStudent(2, "raju", 67, 67, 40);
+            db.AddStudent(3, "ramu", 69, 36, 90);
+            db.AddStudent(4, "rani", 99, 67, 89);
+            db.AddStudent(5, "rama", 89, 99, 90);
+            db.PrintAllStudentNames();
+            db.PrintStudentWithMatchingMark1(99);
+            db.PrintStudentWithMatchingMark2AndMark3(99,99);
+            db.AverageStudentMark();
+            db.StudentOfResult();
+            db.FindStudentGivenMarkRange();
+            db.MaximumMarkOfStudent();
+            db.MinimumMarkOfStudent();
+        }
+    }
+}
---
+++ Raja/StudentDataBaseUsingArrayObjectList/StudentDatabaseUsingArrayList/StudentDatabaseUsingArrayList/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDatabaseUsingArrayList
+{
+    class Student
+    {
+        public int id;
+        public string name;
+        public int mark1;
+        public int mark2;
+        public int mark3;
+    }
+}
