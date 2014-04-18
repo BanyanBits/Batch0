21e0a8345f3c19fa3e2e91e6993224a88eec946c
Rajaramachandran <rajaramachandran88@gmail.com>
2014-04-18 07:57:03 +0530

    Added Some more Method

--- Raja/StudentDataBaseUsingArrayObjectList/Program.cs
+++ Raja/StudentDataBaseUsingArrayObjectList/Program.cs
@@ -13,17 +13,21 @@ namespace StudentDatabaseUsingArrayList
             StudentDb db= new StudentDb(10);
             db.AddStudent(1, "raja", 19, 67, 99);
             db.AddStudent(2, "raju", 67, 67, 40);
-            db.AddStudent(3, "ramu", 69, 36, 90);
+            db.AddStudent(3, "ramu", 69, 36, 78);
             db.AddStudent(4, "rani", 99, 67, 89);
-            db.AddStudent(5, "rama", 89, 99, 90);
+            db.AddStudent(5, "rama", 89, 67, 90);
             db.PrintAllStudentNames();
-            db.PrintStudentWithMatchingMark1(99);
-            db.PrintStudentWithMatchingMark2AndMark3(99,99);
-            db.AverageStudentMark();
-            db.StudentOfResult();
-            db.FindStudentGivenMarkRange();
-            db.MaximumMarkOfStudent();
-            db.MinimumMarkOfStudent();
+            db.PrintAllStudentMark1();
+            //db.PrintStudentWithMatchingMark1(99);
+            //db.PrintStudentWithMatchingMark2AndMark3(99,99);
+            //db.AverageStudentMark();
+            //db.StudentOfResult();
+            //db.FindStudentGivenMarkRange();
+            //db.MaximumMarkOfStudent();
+            //db.MinimumMarkOfStudent();
+            //db.PrintStudentInfoByMark(67);
+            //db.PrintGradeForEachSubject();
+            //db.PrintOfTopStudent();
         }
     }
 }
