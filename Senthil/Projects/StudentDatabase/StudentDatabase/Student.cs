e718237d22bcd2fbbd2855bb5185d5ea284e6885
senpost <senpost@gmail.com>
2014-05-01 00:01:36 -0500

    Added new project for Raja

---
+++ Senthil/Projects/StudentDatabase/StudentDatabase/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDatabase
+{
+    class Student
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public int  Mark1 { get; private set; }
+
+        public Student(int id, string name, int mark1)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Mark1 = mark1;
+        }
+    }
+}
---
+++ Senthil/Projects/StudentDatabase/StudentDatabase/UserInterface.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDatabase
+{
+    class UserInterface
+    {
+        StudentDb db = new StudentDb();
+
+        private void DisplayMainMenu()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("=======================================");
+            Console.WriteLine("Students Database - Application By Raja");
+            Console.WriteLine("=======================================");
+            Console.WriteLine("Please select the options below to proceed");
+            Console.WriteLine(" c.Print Students Count");
+            Console.WriteLine(" a.Add a new student");
+            Console.WriteLine(" q.Exit");
+            Console.Write("Enter your choice:");
+            ConsoleKeyInfo key =  Console.ReadKey();
+            Console.WriteLine(Environment.NewLine);
+            if(key.Key == ConsoleKey.C)
+            {
+                PrintStudentsCount();
+            }
+            if (key.Key == ConsoleKey.A)
+            {
+                AddStudent();
+            }
+            if(key.Key == ConsoleKey.Q)
+            {
+                Environment.Exit(0);
+            }
+        }
+
+        private void AddStudent()
+        {
+            Console.WriteLine("Enter Student Id:");
+            string id = Console.ReadLine();
+
+            Student student = new Student(1, "Raja", 100);
+            db.AddStudent(student);
+        }
+
+        private void PrintStudentsCount()
+        {
+            Console.WriteLine("Total Number Of Students in the database:{0}", db.Count);
+        }
+
+        public void Run()
+        {
+            while(true)
+            {
+                DisplayMainMenu();
+            }
+        }
+
+    }
+}
