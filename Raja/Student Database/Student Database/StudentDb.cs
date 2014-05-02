ea2622b846f3ef1660d74afe3fc28d1ae573b32b
Rajaramachandran <rajaramachandran88@gmail.com>
2014-05-03 00:25:50 +0530

    Added Student with 100 Marks

--- Raja/Student Database/Student Database/StudentDb.cs
+++ Raja/Student Database/Student Database/StudentDb.cs
@@ -17,22 +17,46 @@ namespace Student_Database
 
         private bool StudentExistId(int id)
         {
-            Console.WriteLine("Enter the Id", id);
+            Console.WriteLine("");
             foreach (Student iStudent in students)
             {
                 if (iStudent.Id == id)
                 {
-                 break;
-                }              
-                               
+                    return true;
+                }
             }
             return false;
         }
 
-        public void DeleteStudent()
+        public void DeleteStudent(int id)
         {
-           
-         
+            Console.WriteLine("Enter the Id:");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            foreach (var stu in students)
+            {
+                if (stu.Id == userInput)
+                {
+                    students.RemoveAt(userInput);
+                    Console.WriteLine("Student has been deleted Successfully");
+                }
+            }
+        }
+
+        public void StudentWithTopMark()
+        {
+            Console.WriteLine("Enter the Mark1:");
+            int mark1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("StudentId|StudentName");
+            Console.WriteLine("*********|***********");
+
+            foreach (var iStudent in students)
+            {
+                if (iStudent.Mark1 == mark1)
+                {
+                    Console.WriteLine("{0}       {1}", iStudent.Id, iStudent.Name);
+                }
+            }
         }
 
         public int Count
--- Raja/Student Database/Student Database/UserInterface.cs
+++ Raja/Student Database/Student Database/UserInterface.cs
@@ -20,11 +20,11 @@ namespace Student_Database
             Console.WriteLine("a.Print Students Count");
             Console.WriteLine("c.Add the Student");
             Console.WriteLine("d.Delete the Student");
-            Console.WriteLine("d.Delete the Student");
+            Console.WriteLine("h.Students who scored hunderd marks");
             Console.WriteLine("q.Exit");
             Console.Write("Enter Your Choice");
             ConsoleKeyInfo key = Console.ReadKey();
-            Console.WriteLine(Environment.NewLine);  
+            Console.WriteLine(Environment.NewLine);
             if (key.Key == ConsoleKey.A)
             {
                 PrintStudentcount();
@@ -32,6 +32,10 @@ namespace Student_Database
             if (key.Key == ConsoleKey.C)
             {
                 AddStudent();
+            }
+            if(key.Key==ConsoleKey.H)
+            {
+                StudentWithHunderdMark();
             }
             if (key.Key == ConsoleKey.D)
             {
@@ -43,14 +47,19 @@ namespace Student_Database
             }
         }
 
+        public void StudentWithHunderdMark()
+        {
+            db.StudentWithTopMark();
+        }
+
         public void AddStudent()
         {
             Console.WriteLine("Enter the Id:");
             string userInput = Console.ReadLine();
             int id;
-            if(!Int32.TryParse(userInput,out id))
+            if (!Int32.TryParse(userInput, out id))
             {
-             Console.WriteLine("Invalid id ,Please enter the valid id in positive Integer ");
+                Console.WriteLine("Invalid id ,Please enter the valid id in positive Integer ");
                 return;
             }
             Console.WriteLine("Enter the Name");
@@ -58,22 +67,20 @@ namespace Student_Database
             Console.WriteLine("Enter the Mark1");
             userInput = Console.ReadLine();
             int mark1;
-            if (!Int32.TryParse(userInput, out mark1)||mark1<0||mark1>100)
+            if (!Int32.TryParse(userInput, out mark1) || mark1 < 0 || mark1 > 100)
             {
                 Console.WriteLine("Invalid mark ,Please enter the valid mark between mark<0&&mark>100");
                 return;
-            }            
-            Student student = new Student(id,name,mark1);
+            }
+            Student student = new Student(id, name, mark1);
             db.AddStudent(student);
         }
 
         public void DeleteStudent()
         {
-            
-            db.DeleteStudent();
-
+            db.DeleteStudent(1);
         }
-        
+
         public void PrintStudentcount()
         {
             Console.WriteLine("Total number of student from the Database{0}", db.Count);
